c1a68311232fc5f1bd7fe7c17bf582c904adf24c
Weyland Joyner <weylandjoyner4@gmail.com>
2015-06-11 22:13:49 -0400

    dbs initialized

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route('/')
 def hello():
@@ -11,8 +15,6 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
   return "Hello {}!".format(name)
-
-print(os.environ['APP_SETTINGS'])
 
 if __name__ == '__main__':
   app.run()
