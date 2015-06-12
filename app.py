edec629379596ad68f4790ebd0ba634b9acf292f
Weyland Joyner <weylandjoyner4@gmail.com>
2015-06-12 11:27:10 -0400

    messing with app.py

--- app.py
+++ app.py
@@ -17,46 +17,47 @@ from models import Result
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  errors = []
-  results = {}
-  if request.method == 'POST':
-    try:
-      url = request.form['url']
-      r = requests.get(url)
-      print(r.text)
-    except:
-      errors.append("Unable to get URL. Please make sure it's valid and try again.")
-      return render_template('index.html', errors = errors)
-
-    if r:
-      raw = BeautifulSoup(r.text).get_text()
-      nltk.data.path.append('./nltk_data/')
-      tokens = nltk.word_tokenize(raw)
-      text = nltk.Text(tokens)
-
-      nonPunct = re.compile('.*[A-Za-z].*')
-      raw_words = [w for w in text if nonPunct.match(w)]
-      raw_word_count = Counter(raw_words)
-
-      no_stop_words = [w for w in raw_words if w.lower() not in stops]
-      no_stop_words_count = Counter(no_stop_words)
-
-      results = sorted(
-        no_stop_words_count.items(),
-        key = operator.itemgetter(1),
-        reverse = True
-      )
-      try:
-        result = Result(
-          url = url,
-          result_all = raw_word_count,
-          result_no_stop_words = no_stop_words_count
-        )
-        db.session.add(result)
-        db.session.commit()
-      except:
-        errors.append("Unable to add items to database.")
-  return render_template('index.html', errors=errors, results=results)
+  return("Hello!");
+##  errors = []
+##  results = {}
+##  if request.method == 'POST':
+##    try:
+##      url = request.form['url']
+##      r = requests.get(url)
+##      print(r.text)
+##    except:
+##      errors.append("Unable to get URL. Please make sure it's valid and try again.")
+##      return render_template('index.html', errors = errors)
+##
+##    if r:
+##      raw = BeautifulSoup(r.text).get_text()
+##      nltk.data.path.append('./nltk_data/')
+##      tokens = nltk.word_tokenize(raw)
+##      text = nltk.Text(tokens)
+##
+##      nonPunct = re.compile('.*[A-Za-z].*')
+##      raw_words = [w for w in text if nonPunct.match(w)]
+##      raw_word_count = Counter(raw_words)
+##
+##      no_stop_words = [w for w in raw_words if w.lower() not in stops]
+##      no_stop_words_count = Counter(no_stop_words)
+##
+##      results = sorted(
+##        no_stop_words_count.items(),
+##        key = operator.itemgetter(1),
+##        reverse = True
+##      )
+##      try:
+##        result = Result(
+##          url = url,
+##          result_all = raw_word_count,
+##          result_no_stop_words = no_stop_words_count
+##        )
+##        db.session.add(result)
+##        db.session.commit()
+##      except:
+##        errors.append("Unable to add items to database.")
+##  return render_template('index.html', errors=errors, results=results)
 
 @app.route('/<name>')
 def hello_name(name):
